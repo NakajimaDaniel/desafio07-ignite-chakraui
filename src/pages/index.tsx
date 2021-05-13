25257ec3ef6ffcfbd51bd7fd15849ef4f9bd31c0
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-13 19:50:55 -0300

    components add index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
+import { TravelTypes } from "../components/TravelTypes";
 
 export default function Home() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Flex flexDirection="column">
+        <Banner />
+        <TravelTypes />
+        
+      </Flex>
+    </>
   )
 }
