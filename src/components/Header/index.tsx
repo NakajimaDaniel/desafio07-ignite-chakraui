d77276f530612046c820ce8784d23b210414bd28
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-13 19:50:27 -0300

    header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Flex, Image } from "@chakra-ui/react";
 
 export function Header() {
   return (
-    <Flex as="header" w="100%" justifyContent="center" p="2">
+    <Flex as="header" w="100%" justifyContent="center" pb="8" pt="8">
       <Image src="/logo.png"></Image>
     </Flex>
   )
