5d3a4161ea74af3285acf122d3cfd606759fcbed
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-13 19:50:17 -0300

    banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,11 @@
+
+import  { Image, Container, Flex } from '@chakra-ui/react'
+
+export function Banner() {
+  return (
+    <Flex w="100%" mb="8">
+      <Image src="Banner.png" />
+    </Flex>
+    
+  )
+}
