88bf78e743f62fcd9944043a687cfc9b71ea626e
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-14 18:47:22 -0300

    divider component

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,15 @@
+import { Box, Divider as DividerChakra, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+export function Divider() {
+  return (
+    <Flex justify="center">
+      
+      <Box w="10%" bg="gray.600" h="0.5">
+      
+      </Box>
+    </Flex>
+    
+
+  )
+}
