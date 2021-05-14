35f4566e4b59aa1dbd6b291ac41dff61718cfba6
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-14 18:31:01 -0300

    grid space adj

--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -5,31 +5,31 @@ import React from "react";
 export function TravelTypes() {
   return (
     <Flex justifyContent="center" pl="8" pr="8">
-      <Grid templateColumns="repeat(5, 1fr)" gap="20">
+      <Grid templateColumns="repeat(5, 1fr)" gap="8rem">
 
         <Box align="center">
           <Image src="cocktail.png" />
-          <Text fontWeight="semibold" pt="4">vida noturna</Text>
+          <Text fontWeight="semibold" pt="4" color="gray.600">vida noturna</Text>
         </Box>
 
-        <Box align="center">
+        <Box align="center" w="100%">
           <Image src="surf.png" />
-          <Text fontWeight="semibold" pt="4">praia</Text>
+          <Text fontWeight="semibold" pt="4" color="gray.600">praia</Text>
         </Box>
 
-        <Box align="center">
+        <Box align="center" w="100%">
           <Image src="building.png" />
-          <Text fontWeight="semibold" pt="4">moderno</Text>
+          <Text fontWeight="semibold" pt="4" color="gray.600">moderno</Text>
         </Box>
 
-        <Box align="center">
+        <Box align="center" w="100%">
           <Image src="museum.png" />
-          <Text fontWeight="semibold" pt="4">clássico</Text>
+          <Text fontWeight="semibold" pt="4" color="gray.600">clássico</Text>
         </Box>
 
-        <Box align="center">
+        <Box align="center" w="100%">
           <Image src="earth.png" />
-          <Text fontWeight="semibold" pt="4">e mais ...</Text>
+          <Text fontWeight="semibold" pt="4" color="gray.600">e mais ...</Text>
         </Box>
           
 
