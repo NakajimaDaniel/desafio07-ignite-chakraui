f1f96977340286f1243d8199a6b55dd8c1506eaa
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-14 18:31:16 -0300

    banner width adj

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,7 +4,7 @@ import  { Image, Container, Flex } from '@chakra-ui/react'
 export function Banner() {
   return (
     <Flex w="100%" mb="8">
-      <Image src="Banner.png" />
+      <Image src="Banner.png" w="100%"/>
     </Flex>
     
   )
