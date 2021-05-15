5281053fcfbf625d14441a16422b00a7a5951213
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-15 16:46:05 -0300

    img size adj responsive

--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -5,30 +5,30 @@ import React from "react";
 export function TravelTypes() {
   return (
     <Flex justifyContent="center" pl="8" pr="8" pb="8" mb="8">
-      <Grid templateColumns="repeat(5, 1fr)" gap="1" w="100vw">
+      <Grid templateColumns="repeat(5, 1fr)" gap={[3,1]} w="100vw">
 
-        <Box align="center">
-          <Image src="cocktail.png" boxSize="10" />
+        <Box align="center">  
+          <Image src="cocktail.png" boxSize={['10px','30px','50px','70px']} />
           <Text fontWeight="semibold" pt="4" color="gray.600">vida noturna</Text>
         </Box>
 
         <Box align="center">
-          <Image src="surf.png" boxSize="10"/>
+          <Image src="surf.png" boxSize={['10px','30px','50px','70px']}/>
           <Text fontWeight="semibold" pt="4" color="gray.600">praia</Text>
         </Box>
 
         <Box align="center">
-          <Image src="building.png" boxSize="10"/>
+          <Image src="building.png" boxSize={['10px','30px','50px','70px']}/>
           <Text fontWeight="semibold" pt="4" color="gray.600">moderno</Text>
         </Box>
 
         <Box align="center" >
-          <Image src="museum.png" boxSize="10"/>
+          <Image src="museum.png" boxSize={['10px','30px','50px','70px']}/>
           <Text fontWeight="semibold" pt="4" color="gray.600">clássico</Text>
         </Box>
 
         <Box align="center" >
-          <Image src="earth.png" boxSize="10"/>
+          <Image src="earth.png" boxSize={['10px','30px','50px','70px']}/>
           <Text fontWeight="semibold" pt="4" color="gray.600">e mais ...</Text>
         </Box>
           
