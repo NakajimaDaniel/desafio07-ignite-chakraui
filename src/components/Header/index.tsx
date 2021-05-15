8f38982807ebdfcb6dd677ad4f3975ba7cb35bc4
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-15 16:33:49 -0300

    padding adj

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ import { Flex, Image } from "@chakra-ui/react";
 
 export function Header() {
   return (
-    <Flex as="header" w="100%" justifyContent="center" pb="8" pt="8">
-      <Image src="/logo.png"></Image>
+    <Flex as="header" w="100%" justifyContent="center" pb="6" pt="6">
+      <Image src="/logo.png"/>
     </Flex>
   )
 }
