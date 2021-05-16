518dc2e4cac4f0fdf290851028e774b9bb210a79
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-16 16:19:38 -0300

    header link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import { Flex, Image } from "@chakra-ui/react";
+import Link from 'next/link'
+
 
 export function Header() {
   return (
     <Flex as="header" w="100%" justifyContent="center" pb="6" pt="6">
-      <Image src="/logo.png"/>
+      <Link href="/">
+        <Image src="/logo.png" cursor="pointer" />
+      </Link>  
     </Flex>
   )
 }
