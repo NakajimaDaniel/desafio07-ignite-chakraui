de5e940117dcffcac6d1e471bcad274892c350f8
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-19 20:39:01 -0300

    remove static banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,10 +1,17 @@
 
-import  { Image, Container, Flex } from '@chakra-ui/react'
+import  { Image, Container, Flex, Box, Text } from '@chakra-ui/react'
 
 export function Banner() {
   return (
     <Flex w="100%" mb="8">
-      <Image src="Banner.png" w="100%"/>
+      <Box position="relative">
+        <Image src="bannerback.png" w="100vw"/>
+        <Text position="absolute" top="20%"  left="10%" color="white" fontSize={["20","20","20","40"]}>5 Continentes,</Text>
+        <Text position="absolute" top="35%"  left="10%" color="white"  fontSize={["20","20","20","40"]} >infinitas possibilidades.</Text>
+        <Text position="absolute" top="55%"  left="10%" color="gray.500"  fontSize={["10","10","20"]} maxW={[300,300,400,500]}>Chegou a hora de tirar do papel a viagem que você sempre sonhou. </Text>
+        <Image position="absolute" top="20%" right="10%" src="Airplane.png" w="30%" />
+      </Box>
+      
     </Flex>
     
   )
