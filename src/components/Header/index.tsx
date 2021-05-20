6eaacd277e83555d2cd7b1647800d915e5601ea8
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-20 19:49:49 -0300

    image size adj

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex, Image, useMediaQuery } from "@chakra-ui/react";
 import Link from 'next/link'
 
 
 export function Header() {
+
+  const [isLargerThan500] = useMediaQuery("(min-width: 500px)")
+
   return (
     <Flex as="header" w="100%" justifyContent="center" pb="6" pt="6">
       <Link href="/">
-        <Image src="/logo.png" cursor="pointer" />
+        {isLargerThan500? (
+          <Image src="/logo.png" cursor="pointer" />
+        ): (
+          <Image src="/logo.png" cursor="pointer" boxSize="25%" />
+        )}
+        
       </Link>  
     </Flex>
   )
