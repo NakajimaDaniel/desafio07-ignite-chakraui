10d83b511ae9adf9becc78919f94ccb731fa8a1c
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-21 19:19:03 -0300

    travel types responsive sx props

--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -11,36 +11,114 @@ export function TravelTypes() {
     <>
 
      
-          <Flex justifyContent="center" pl={['4','6', '8']} pr={['4','6', '8']} pb={['4','6', '8']} mb={['4','6', '8']}>
-            <Grid templateColumns="repeat(5, 1fr)" gap={[1,3]} w="100vw">
+      <Flex
+        justifyContent="center"
+        pl={['4','6',
+        '8']}
+        pr={['4','6',
+        '8']}
+        pb={['4','6',
+        '8']}
+        mb={['4','6',
+        '8']}
+        >
+        <Grid
+          templateColumns="repeat(5, 1fr)"
+          gap={[1,3]}
+          w="100vw"
+          sx={{"@media screen and (max-width: 700px)": {
+            gridTemplateColumns: 'repeat(2, 1fr)',
+          }}}
+        >
 
-              <Box align="center">  
-                <Image src="cocktail.png" boxSize={['10px','30px','50px','70px']} />
-                <Text fontWeight="semibold" pt="4" color="gray.600">vida noturna</Text>
-              </Box>
+          <Box align="center">  
+            <Image
+              src="cocktail.png"
+              boxSize={['10px','30px','50px','70px']} 
+              sx={{"@media screen and (max-width: 700px)": {
+                display: 'none'
+              }}}
+            />
+            <Text
+              fontWeight="semibold"
+              pt="4"
+              color="gray.600"
+            >
+              vida
+              noturna
+            </Text>
+          </Box>
 
-              <Box align="center">
-                <Image src="surf.png" boxSize={['10px','30px','50px','70px']}/>
-                <Text fontWeight="semibold" pt="4" color="gray.600">praia</Text>
-              </Box>
+          <Box align="center">
+            <Image
+              src="surf.png"
+              boxSize={['10px','30px','50px','70px']} 
+              sx={{"@media screen and (max-width: 700px)": {
+                display: 'none'
+              }}}
+            />
+            <Text
+              fontWeight="semibold"
+              pt="4"
+              color="gray.600"
+            >
+              praia
+            </Text>
+          </Box>
 
-              <Box align="center">
-                <Image src="building.png" boxSize={['10px','30px','50px','70px']}/>
-                <Text fontWeight="semibold" pt="4" color="gray.600">moderno</Text>
-              </Box>
+          <Box align="center">
+            <Image
+              src="building.png"
+              boxSize={['10px','30px','50px','70px']} 
+              sx={{"@media screen and (max-width: 700px)": {
+                display: 'none'
+              }}}
+            />
+            <Text
+              fontWeight="semibold"
+              pt="4"
+              color="gray.600"
+            >
+              moderno
+            </Text>
+          </Box>
 
-              <Box align="center" >
-                <Image src="museum.png" boxSize={['10px','30px','50px','70px']}/>
-                <Text fontWeight="semibold" pt="4" color="gray.600">clássico</Text>
-              </Box>
+          <Box align="center" >
+            <Image
+              src="museum.png"
+              boxSize={['10px','30px','50px','70px']} 
+              sx={{"@media screen and (max-width: 700px)": {
+                display: 'none'
+              }}}
+            />
+            <Text
+              fontWeight="semibold"
+              pt="4"
+              color="gray.600"
+            >
+              clássico
+            </Text>
+          </Box>
 
-              <Box align="center" >
-                <Image src="earth.png" boxSize={['10px','30px','50px','70px']}/>
-                <Text fontWeight="semibold" pt="4" color="gray.600">e mais ...</Text>
-              </Box>
+          <Box align="center" >
+            <Image
+              src="earth.png"
+              boxSize={['10px','30px','50px','70px']} 
+              sx={{"@media screen and (max-width: 700px)": {
+                display: 'none'
+              }}}
+            />
+            <Text
+              fontWeight="semibold"
+              pt="4"
+              color="gray.600"
+            >
+              e mais...
+            </Text>
+          </Box>
                 
-            </Grid>
-          </Flex>
+        </Grid>
+      </Flex>
 
 
     </>
