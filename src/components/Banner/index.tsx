6b6de01f59c8a6fdde82a634a4d156472f8e2e3f
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-21 18:26:57 -0300

    removed mediaquery hook

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,35 +1,57 @@
 
-import  { Image, Container, Flex, Box, Text, useMediaQuery } from '@chakra-ui/react'
+import  { Image, Container, Flex, Box, Text} from '@chakra-ui/react'
 
 
 
 export function Banner() {
 
-  const [isLargerThan700] = useMediaQuery("(min-width: 700px)")
-
-
-  
+ 
   return (
     <Flex w="100%" mb="8" pb="10">
-      <Box position="relative">
-        
-        {isLargerThan700? (
-          <>
-            <Image src="bannerback.png" w="100vw"/>
-            <Text position="absolute" top="20%"  left="10%" color="white" fontSize={["20","20","20","40"]}>5 Continentes,</Text>
-            <Text position="absolute" top="35%"  left="10%" color="white"  fontSize={["20","20","20","40"]} >infinitas possibilidades.</Text>
-            <Text position="absolute" top="55%"  left="10%" color="gray.500"  fontSize={["10","10","20"]} maxW={[300,300,400,500]}>Chegou a hora de tirar do papel a viagem que você sempre sonhou. </Text>
-            <Image position="absolute" top="20%" right="10%" src="Airplane.png" w="30%" />
-          </>
-
-        ): (
-          <>
-            <Image src="bannerback.png" w="100vw" h={[200]}/>
-            <Text position="absolute" top="20%"  left="7%" color="white" fontSize={["20"]}>5 Continentes,</Text>
-            <Text position="absolute" top="35%"  left="7%" color="white"  fontSize={["20"]} >infinitas possibilidades.</Text>
-            <Text position="absolute" top="60%"  left="7%" color="gray.500"  fontSize={["15"]} w="90vw">Chegou a hora de tirar do papel a viagem que você sempre sonhou. </Text>
-          </>
-        )}
+      <Box position="relative" sx={{
+        "@media screen and (max-width: 375px)": {
+            height: "100vh",
+        },
+      }}>
+   
+          <Image src="bannerback.png" w="100vw"/>
+          <Text 
+            position="absolute" 
+            top="20%"  
+            left="10%" 
+            color="white" 
+            fontSize={["20","20","20","40"]}
+          >
+              5 Continentes,
+          </Text>
+          <Text
+            position="absolute" 
+            top="35%"  
+            left="10%" 
+            color="white"  
+            fontSize={["20","20","20","40"]} 
+           >
+             infinitas possibilidades.
+          </Text>
+          <Text 
+            position="absolute" 
+            top="55%"  left="10%" 
+            color="gray.500"  
+            fontSize={["10","10","20"]} 
+            maxW={[300,300,400,500]}
+          >
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou. 
+          </Text>
+          <Image 
+            position="absolute" 
+            top="20%" 
+            right="10%" 
+            src="Airplane.png" 
+            w="30%" 
+            sx={{"@media screen and (max-width:700px)": {
+              display: 'none',
+            }}}
+          />
 
       </Box>
       
@@ -37,3 +59,10 @@ export function Banner() {
     
   )
 }
+
+<>
+<Image src="bannerback.png" w="100vw" h={[200]}/>
+<Text position="absolute" top="20%"  left="7%" color="white" fontSize={["20"]}>5 Continentes,</Text>
+<Text position="absolute" top="35%"  left="7%" color="white"  fontSize={["20"]} >infinitas possibilidades.</Text>
+<Text position="absolute" top="60%"  left="7%" color="gray.500"  fontSize={["15"]} w="90vw">Chegou a hora de tirar do papel a viagem que você sempre sonhou. </Text>
+</>
