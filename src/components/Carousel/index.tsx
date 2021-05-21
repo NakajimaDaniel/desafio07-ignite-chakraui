294affd27dfdd6bc99b4d2b1fa1b71c5d1a2ccd1
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-05-21 19:44:59 -0300

    carousel responsive sx props

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -33,7 +33,18 @@ export function Carousel() {
   return (
     <>
     
-      <Flex justify="center" mt={['10','15', '15']} ml={['10']} mr={['10']} mb={['20']} >
+      <Flex
+        justify="center"
+        mt={['10','15',
+        '15']}
+        ml={['10']}
+        mr={['10']}
+        mb={['20']}
+        sx={{"@media screen and (max-width: 700px)": {
+          mr: '0',
+          ml: '0',
+        }}}
+      >
         <Swiper  navigation={true}  slidesPerView={1}  centeredSlides={true} pagination={{
         "clickable": true}}>
           <SwiperSlide>
@@ -41,8 +52,47 @@ export function Carousel() {
               <Box position="relative" textAlign="center" cursor="pointer"> 
               
                 <Image src="europe.png" w="100%" />
-                <Text position="absolute" top="50%" left="50%" transform="translate(-50%,-50%)" color="white" fontSize={['1xl','2xl', '3xl','6xl']}  fontWeight="bold">Europa</Text>
-                <Text position="absolute" top="50%" left="50%" transform={["translate(-50%, 80%)","translate(-50%, 100%)","translate(-50%, 110%)","translate(-50%, 160%)"]} color="white" fontSize={['md','md','md','3xl']} fontWeight="bold">O continente mais antigo.</Text>
+                <Text
+                  position="absolute"
+                  top="50%"
+                  left="50%"
+                  transform="translate(-50%,-50%)"
+                  color="white"
+                  fontSize={['1xl','2xl',
+                  '3xl','6xl']}
+                  fontWeight="bold"
+                  sx={{"@media screen and (max-width: 600px)": {
+                    transform: 'translate(-50%, -80%)'
+                  },
+                    "@media screen and (max-width: 490px)": {
+                      transform: 'translate(-50%, -130%)',
+                      fontSize: '2xl'
+                    }
+                  }}
+                >
+                  Europa
+                </Text>
+                <Text
+                  position="absolute"
+                  top="50%"
+                  left="50%"
+                  transform={["translate(-50%, 80%)","translate(-50%, 100%)","translate(-50%, 110%)","translate(-50%, 160%)"]}
+                  color="white"
+                  fontSize={['md','md','md','3xl']}
+                  fontWeight="bold"
+                  sx={{"@media screen and (max-width: 600px)": {
+                    transform: 'translate(-50%, 80%)'
+                  },
+                    "@media screen and (max-width: 490px)": {
+                      transform: 'translate(-50%, 0%)'
+                    }
+                  }}
+                >
+                    O
+                  continente
+                  mais
+                  antigo.
+                </Text>
 
               </Box>
             </Link>
