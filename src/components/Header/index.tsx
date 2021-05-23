7dcb4818ff901894b6774342b35c8ed43d84101f
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-05-23 13:09:49 -0300

    button adj

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,30 @@
-import { Flex, Image, useMediaQuery } from "@chakra-ui/react";
+import { Flex, Image, useMediaQuery, Box } from "@chakra-ui/react";
 import Link from 'next/link'
-
+import {useRouter} from 'next/router'
 
 export function Header() {
 
+  const router = useRouter();
   const [isLargerThan500] = useMediaQuery("(min-width: 500px)")
 
   return (
     
     <> 
-      <Image  position="absolute"  left="10%" transform="translate(-50%, 100%)" src="return.png" />
+      <Box display={router.asPath === '/'? 'none': 'block'} >
+        <Link href="/">
+          <Image
+            position="absolute"
+            left="10%"
+            transform="translate(-50%, 100%)"
+            src="return.png" 
+            sx={{"@media screen and (max-width: 500px)": {
+              transform: 'translate(-50%, 80%)'
+            }}}
+            cursor="pointer"
+          />
+        </Link>
+      </Box>
+      
       <Flex as="header" w="100%" justifyContent="center" pb="6" pt="6">
         <Link href="/">
           {isLargerThan500? (
