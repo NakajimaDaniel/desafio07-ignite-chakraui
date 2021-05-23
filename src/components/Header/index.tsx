b089f45baf9c88129f2230e9b839f262c70c443f
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-05-23 11:18:51 -0300

    return button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,15 +7,20 @@ export function Header() {
   const [isLargerThan500] = useMediaQuery("(min-width: 500px)")
 
   return (
-    <Flex as="header" w="100%" justifyContent="center" pb="6" pt="6">
-      <Link href="/">
-        {isLargerThan500? (
-          <Image src="/logo.png" cursor="pointer" />
-        ): (
-          <Image src="/logo.png" cursor="pointer" boxSize="25%" />
-        )}
-        
-      </Link>  
-    </Flex>
+    
+    <> 
+      <Image  position="absolute"  left="10%" transform="translate(-50%, 100%)" src="return.png" />
+      <Flex as="header" w="100%" justifyContent="center" pb="6" pt="6">
+        <Link href="/">
+          {isLargerThan500? (
+            <Image src="/logo.png" cursor="pointer" />
+          ): (
+            <Image src="/logo.png" cursor="pointer" boxSize="25%" />
+          )}
+          
+        </Link>  
+      </Flex>
+    </>
+
   )
 }
